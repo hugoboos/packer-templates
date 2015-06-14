b6aa122263833cd801b7209fe4355037929810b5
Hugo Stijns <hugo@boosboos.net>
2015-06-14 21:08:10 +0200

    Changed partition layout to be CIS compliant

--- scripts/compact.sh
+++ scripts/compact.sh
@@ -1,16 +1,16 @@
 # Clear the swap memory
 SWAP_UUID=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
 SWAP_PARTITION=$(readlink -f /dev/disk/by-uuid/"${SWAP_UUID}")
+MOUNTS=("/" "/boot" "/home" "/var" "/var/log" "/var/log/audit" "/tmp")
 
 swapoff ${SWAP_PARTITION}
 dd if=/dev/zero of=${SWAP_PARTITION} bs=1M
 mkswap -U ${SWAP_UUID} ${SWAP_PARTITION}
 
 # Zero out the drive for better compression
-dd if=/dev/zero of=/boot/EMPTY bs=1M
-rm -f /boot/EMPTY
-
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+for MOUNT in ${MOUNTS[@]}; do
+  dd if=/dev/zero of=${MOUNT}/EMPTY bs=1M
+  rm -f ${MOUNT}/EMPTY
+done
 
 sync
